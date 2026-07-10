9d6c4c2438dcf06ddebf3c9393895cff10733d78
Benjamin Eskildsen <eskildsen.ben@gmail.com>
2026-07-10 11:16:39 -0400

    Basic Sensor page and pulling in readings

--- Models/TemperatureModels.cs
+++ Models/TemperatureModels.cs
@@ -17,4 +17,18 @@ namespace BlazorTemps.Models
         public double? MinTempF { get; set; }
         public double? MaxTempF { get; set; }
     }
+
+    public sealed class ReadingsResponseDTO
+    {
+        public required List<ReadingDTO> Readings { get; set; }
+    }
+
+    public sealed class ReadingDTO
+    {
+        public int ReadingID { get; set; }
+        public double TempF { get; set; }
+        public double Humidity { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int SensorID { get; set; }
+    }
 }
